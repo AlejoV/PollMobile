9036c85e5583a9375cacf04a3f69f156761285f7
Alejandro Valencia <alejo.valencia@gmail.com>
2015-04-28 21:09:16 +0930

    Initial Commit

---
+++ PollMobile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PollMobile
+//
+//  Created by Alejandro Valencia I on 28/04/2015.
+//  Copyright (c) 2015 com.asoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
